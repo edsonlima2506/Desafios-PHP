f7701f2217631ae0613df9fccb0693fc6a7d2c77
edson <edsoneduardo526@gmail.com>
2022-02-23 09:55:30 -0300

    Desafio 9 completo

---
+++ desafio9.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ Entrar com o número de vezes que se deseja
+imprimir a palavra SOL e imprimir
+*/
+
+$vezes = 10;
+
+for ($i = 1; $i <= $vezes; $i += 1) {
+    echo("$i - SOL <br>");
+}
