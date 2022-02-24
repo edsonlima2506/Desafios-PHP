d685fe128dcad266bb92efc9c88a7ef46f85fe99
edson <edsoneduardo526@gmail.com>
2022-02-23 21:13:10 -0300

    Desafio 13 completo

---
+++ desafio13.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ Chico tem 1,50m e cresce 2 centímetros por
+ano, enquanto Juca tem 1,10m e cresce 3
+centímetros por ano. Construir um algoritmos
+que calcule e imprima quantos anos serão
+necessários para que Juca seja maior que
+Chico.
+*/
+
+$chico = 1.50;
+$juca = 1.10;
+$contador = 0;
+
+do {
+    $chico += 0.02;
+    $juca += 0.03;
+    $contador ++;
+} while ($juca < $chico);
+
+echo($contador);
